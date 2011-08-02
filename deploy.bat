0c770750fa3b8377c67cfd72868f0d6d31850601
alllee <allen.lee@asu.edu>
2011-08-02 13:19:15 -0700

    updating ant + ivy initialization - should consider consolidating deployment to one build tool instead of having ant + maven + ivy

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,6 @@ REM set ANT_HOME to the location where you unpacked Ant.  Inside this
 REM directory should be bin and lib directories containing the Ant
 REM executables and library dependencies.
 set ANT_HOME=C:\tools\ant
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_10
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
 set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
 ant deploy
