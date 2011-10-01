d2c60f943015fe8c4fa62a4eb4def6aded5c0fe1
alllee <allen.lee@asu.edu>
2011-10-01 02:12:11 -0700

    && appears to be working to daisy chain the mvn and ant commands.  buildr is looking more attractive by the minute, though comes with its own dependency issues, namely installing ruby on windows, etc.

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
-REM set ANT_HOME to the location where you unpacked Ant.  Inside this
-REM directory should be bin and lib directories containing the Ant
-REM executables and library dependencies.
-REM set JAVA_HOME to the location of the installed JDK (customize as necessary)
+::REM set ANT_HOME to the location where you unpacked Ant.  Inside this
+::REM directory should be bin and lib directories containing the Ant
+::REM executables and library dependencies.
+::REM set JAVA_HOME to the location of the installed JDK (customize as necessary)
+REM This script will not work unless you make sure that ANT_HOME, M2_HOME, and JAVA_HOME are pointing at the correct location.
 set ANT_HOME=C:\tools\ant
 set M2_HOME=C:\tools\maven
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
 set PATH=%M2_HOME%\bin;%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
-mvn clean compile & ant clean deploy server
+mvn clean compile && ant clean deploy server
