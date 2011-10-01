76f2670f0e94b2137f156d9202010cdf3f2ef82d
alllee <allen.lee@asu.edu>
2011-09-30 23:03:42 -0700

    updating jdk to 1.6.0_26

--- jetty.bat
+++ jetty.bat
@@ -6,6 +6,6 @@ REM You MUST RUN deploy.bat or server.bat before this to properly compile and
 REM configure this application.
 set ANT_HOME=C:\tools\ant
 set M2_HOME=C:\tools\maven
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_10
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%M2_HOME%\bin;%PATH%
 mvn jetty:run
