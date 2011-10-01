fb6263ebc5bbb0455e5794eae08e98f3709736e7
alllee <allen.lee@asu.edu>
2011-09-30 23:01:06 -0700

    having deploy.bat start the server as well.

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,8 @@
 REM set ANT_HOME to the location where you unpacked Ant.  Inside this
 REM directory should be bin and lib directories containing the Ant
 REM executables and library dependencies.
+REM set JAVA_HOME to the location of the installed JDK (customize as necessary)
 set ANT_HOME=C:\tools\ant
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
 set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
-ant clean deploy
+ant clean deploy server
