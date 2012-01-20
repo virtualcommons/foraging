456c9785ec2693760f7d4ceeaae9b6be4f382d42
alllee <none@none>
2012-01-19 23:59:43 -0700

    adding continue button to survey instructions + ActionListener that simply jumps to the regular round instructions and a SurveyCompletedEvent that the client will use to signal to the server + facilitator that the survey has been "completed".  Still need to wire up the server / facilitator handling of SurveyCompletedEvent.

---
+++ src/main/java/edu/asu/commons/foraging/event/SurveyCompletedEvent.java
@@ -0,0 +1,29 @@
+package edu.asu.commons.foraging.event;
+
+import edu.asu.commons.event.AbstractPersistableEvent;
+import edu.asu.commons.event.ClientRequest;
+import edu.asu.commons.net.Identifier;
+
+/**
+ * $Id: QuizResponseEvent.java 522 2010-06-30 19:17:48Z alllee $
+ * 
+ * Signals that a client has completed the survey for the given round.
+ * 
+ * 
+ * @author <a href='mailto:Allen.Lee@asu.edu'>Allen Lee</a>
+ * @version $Rev: 522 $
+ */
+public class SurveyCompletedEvent extends AbstractPersistableEvent implements ClientRequest {
+
+    private static final long serialVersionUID = -7081410122722056083L;
+
+    public SurveyCompletedEvent(Identifier id) {
+        super(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Survey completed by %s", id);
+    }
+    
+}
