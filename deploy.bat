1efade60563085343a6d9a4d0658430e47f9244b
Ben Saylor <brsaylor@gmail.com>
2013-12-09 15:25:02 -0900

    Update deploy.bat and jetty.bat, add process-output-data.bat for Jim's laptop

--- deploy.bat
+++ deploy.bat
@@ -3,8 +3,8 @@
 ::REM executables and library dependencies.
 ::REM set JAVA_HOME to the location of the installed JDK (customize as necessary)
 REM This script will not work unless you make sure that ANT_HOME, M2_HOME, and JAVA_HOME are pointing at the correct location.
-set ANT_HOME=C:\tools\ant
-set M2_HOME=C:\tools\maven
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_31
+set ANT_HOME=C:\JavaLibraries\apache-ant-1.9.2
+set M2_HOME=C:\JavaLibraries\apache-maven-3.1.1
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
 set PATH=%M2_HOME%\bin;%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
-ant clean deploy server
+ant deploy server
--- jetty.bat
+++ jetty.bat
@@ -4,8 +4,8 @@ REM executables and library dependencies.
 REM *******
 REM You MUST RUN deploy.bat or server.bat before this to properly compile and
 REM configure this application.
-set ANT_HOME=C:\tools\ant
-set M2_HOME=C:\tools\maven
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26
+set ANT_HOME=C:\JavaLibraries\apache-ant-1.9.2
+set M2_HOME=C:\JavaLibraries\apache-maven-3.1.1
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%M2_HOME%\bin;%PATH%
 mvn jetty:run
