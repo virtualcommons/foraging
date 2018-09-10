3508f68d09bcce23fa902e55549f3c67a6ae44a1
Allen Lee <allen.lee@asu.edu>
2018-09-10 16:16:25 -0700

    update filename, only supports bot summary data

--- process-bot-data.sh
+++ process-bot-data.sh
@@ -5,6 +5,6 @@ DATA_DIR=${1:-incoming}
 echo "Processing data in ${DATA_DIR}"
 
 ant convert -Ddata.dir=${DATA_DIR} -Dbots=true >| process-bot-data.log
-src/main/python/aggregate.py ${DATA_DIR}
+src/main/python/aggregate-bot-data.py ${DATA_DIR}
 
 echo "Summary results should be in ${DATA_DIR}/*.csv"
