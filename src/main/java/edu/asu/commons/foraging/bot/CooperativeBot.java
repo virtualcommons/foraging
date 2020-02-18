6f8848e6b7bad4647d7d9aa177a8dce6c75d7751
Allen Lee <allen.lee@asu.edu>
2020-02-18 16:04:50 -0700

    keep bot actions per second to 10

--- src/main/java/edu/asu/commons/foraging/bot/CooperativeBot.java
+++ src/main/java/edu/asu/commons/foraging/bot/CooperativeBot.java
@@ -3,7 +3,7 @@ package edu.asu.commons.foraging.bot;
 public class CooperativeBot extends Bot.SimpleBot {
 
     private static final long serialVersionUID = 4732276788271013068L;
-    public final static int ACTIONS_PER_SECOND = 3;
+    public final static int ACTIONS_PER_SECOND = 10;
     public final static double HARVEST_PROBABILITY = 0.2d;
     public final static double MOVEMENT_PROBABILITY = 0.8d;
 
