611f704ed32f879fb6c556039e3b070414070072
Allen Lee <allen.lee@asu.edu>
2021-03-06 23:56:50 -0700

    fix: remove total tokens from output
    - currently bugged and displaying double total collected tokens for some
    reason

--- src/main/java/edu/asu/commons/foraging/data/ResourceSummaryProcessor.java
+++ src/main/java/edu/asu/commons/foraging/data/ResourceSummaryProcessor.java
@@ -4,7 +4,9 @@ import edu.asu.commons.event.PersistableEvent;
 import edu.asu.commons.experiment.SaveFileProcessor;
 import edu.asu.commons.experiment.SavedRoundData;
 import edu.asu.commons.foraging.conf.RoundConfiguration;
+import edu.asu.commons.foraging.event.MovementEvent;
 import edu.asu.commons.foraging.event.SanctionAppliedEvent;
+import edu.asu.commons.foraging.event.TokenCollectedEvent;
 import edu.asu.commons.foraging.model.ClientData;
 import edu.asu.commons.foraging.model.GroupDataModel;
 import edu.asu.commons.foraging.model.ServerDataModel;
@@ -17,6 +19,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * FIXME: Duplicates a fair amount of SummaryProcessor
@@ -28,6 +31,7 @@ public class ResourceSummaryProcessor extends SaveFileProcessor.Base {
     @Override
     public void process(SavedRoundData savedRoundData, PrintWriter writer) {
         ServerDataModel serverDataModel = (ServerDataModel) savedRoundData.getDataModel();
+        Map<Identifier, ClientMovementTokenCount> clientMovementTokenCounts = ClientMovementTokenCount.createMap(serverDataModel);
         serverDataModel.reinitialize((RoundConfiguration) savedRoundData.getRoundParameters());
         List<GroupDataModel> groups = serverDataModel.getOrderedGroups();
         for (PersistableEvent event: savedRoundData.getActions()) {
@@ -39,9 +43,20 @@ public class ResourceSummaryProcessor extends SaveFileProcessor.Base {
                 ClientData target = serverDataModel.getClientData(sanctionEvent.getTarget());
                 target.addSanctionPenalties(sanctionEvent.getSanctionPenalty());
             }
+            else if (event instanceof MovementEvent) {
+                MovementEvent movementEvent = (MovementEvent) event;
+                serverDataModel.apply(movementEvent);
+                ClientMovementTokenCount client = clientMovementTokenCounts.get(event.getId());
+                client.moves++;
+            }
+            else if (event instanceof TokenCollectedEvent) {
+                TokenCollectedEvent tokenCollectedEvent = (TokenCollectedEvent) event;
+                ClientMovementTokenCount client = clientMovementTokenCounts.get(event.getId());
+                client.tokens++;
+            }
         }
 
-        writer.println("Group ID, Group UUID, Participant UUID, Assigned Number, Current Tokens, Total Cumulative Tokens, Sanction costs, Sanction penalties, Group Resources Left");
+        writer.println("Group ID, Group UUID, Participant UUID, Assigned Number, Current Tokens, Moves, Sanction costs, Sanction penalties, Group Resources Left");
         String dateTimeString = extractDateTime(savedRoundData.getSaveFilePath());
         for (GroupDataModel group: groups) {
             ArrayList<ClientData> clientDataList = new ArrayList<>(group.getClientDataMap().values());
@@ -49,13 +64,14 @@ public class ResourceSummaryProcessor extends SaveFileProcessor.Base {
             String groupId = String.format("group-%s_%s", group.getGroupId(), dateTimeString);
             int resourcesLeft = group.getResourceDistributionSize();
             for (ClientData data : clientDataList) {
+                ClientMovementTokenCount cmt = clientMovementTokenCounts.get(data.getId());
                 writer.println(Utils.join(',',
                         groupId,
                         group.getUUID(),
                         data.getId().getUUID(),
                         data.getAssignedNumber(),
-                        data.getCurrentTokens(),
-                        data.getTotalTokens(),
+                        cmt.tokens,
+                        cmt.moves,
                         data.getSanctionCosts(),
                         data.getSanctionPenalties(),
                         resourcesLeft
